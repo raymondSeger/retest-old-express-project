0ed9652fc1611ca4286532a62bb0e1d28efd32dd
Raymond Seger <raymondseger@live.com>
2018-06-20 15:09:26 +0700

    test path

--- routes/routes1.js
+++ routes/routes1.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path   = require('path');
 var router = express.Router();
 
 // middleware that is specific to this router
@@ -9,6 +10,8 @@ router.use(function timeLog (req, res, next) {
 
 // define the home page route
 router.get('/admin/page1', function (req, res) {
+    var a = __dirname;
+    var b = path.join(__dirname, 'public');
     res.send('Admin page 1 page');
 });
 
